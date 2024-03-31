af362390dd75219467f795ee9fe7f6f97b0e311e
Vaibhav <68665948+dead8309@users.noreply.github.com>
2024-03-31 13:19:57 +0530

    update url

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
     links: {
-        challanges: '/challanges',
-        terminal: '/terminal',
+        challanges: 'https://youtu.be/dQw4w9WgXcQ?si=Dwdn0IYfmnCE3xdD',
+        terminal: 'https://shell.d3fc0n.tech',
     },
     endDate: new Date("2024-04-01")
 }
